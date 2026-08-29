5df0e1c9d5708b1fa05532bb5fd1f48319ab5b31
soenneker <jake@soenneker.com>
2026-08-29 14:16:55 -0500

    Improve public API XML documentation

--- src/Soenneker.Box.HttpClients/Abstract/IBoxOpenApiHttpClient.cs
+++ src/Soenneker.Box.HttpClients/Abstract/IBoxOpenApiHttpClient.cs
@@ -11,9 +11,9 @@ namespace Soenneker.Box.HttpClients.Abstract;
 public interface IBoxOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
     /// <summary>
-    /// Gets the value.
+    /// Returns the configured http Client used by the box open api http client.
     /// </summary>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>A task containing the result of the operation.</returns>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested http Client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Box.HttpClients/Registrars/BoxOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Box.HttpClients/Registrars/BoxOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class BoxOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="BoxOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddBoxOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class BoxOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="BoxOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddBoxOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
