fb9d25e1f73bace0c85ac472be0956aa66ebdf61
soenneker <jake@soenneker.com>
2026-08-29 14:50:21 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Box.HttpClients/BoxOpenApiHttpClient.cs
+++ src/Soenneker.Box.HttpClients/BoxOpenApiHttpClient.cs
@@ -11,7 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Box.HttpClients;
 
-///<inheritdoc cref="IBoxOpenApiHttpClient"/>
+/// <inheritdoc cref="IBoxOpenApiHttpClient"/>
 public sealed class BoxOpenApiHttpClient : IBoxOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
